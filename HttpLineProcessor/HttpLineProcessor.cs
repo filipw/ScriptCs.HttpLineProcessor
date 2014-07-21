5a42710f08a9924b55abaa16071611095c0a9203
Filip Wojcieszyn <filip.wojcieszyn@arcmedia.ch>
2014-07-21 08:35:56 +0200

    first commit

---
+++ HttpLineProcessor/HttpLineProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using ScriptCs.Contracts;
+
+namespace HttpLineProcessor
+{
+    public class HttpLineProcessor : DirectiveLineProcessor
+    {
+        protected override BehaviorAfterCode BehaviorAfterCode
+        {
+            get { return BehaviorAfterCode.Allow; }
+        }
+
+        protected override bool ProcessLine(IFileParser parser, FileParserContext context, string line)
+        {
+            var url = GetDirectiveArgument(line);
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(url).Result;
+
+            parser.ParseScript(response.Split(Environment.NewLine.ToCharArray()).ToList(), context);
+
+            return true;
+        }
+
+        protected override string DirectiveName
+        {
+            get { return "http"; }
+        }
+    }
+}
---
+++ HttpLineProcessor/HttpLineProcessorModule.cs
@@ -0,0 +1,13 @@
+﻿using ScriptCs.Contracts;
+
+namespace HttpLineProcessor
+{
+    [Module("httpprocessor")]
+    public class HttpLineProcessorModule : IModule
+    {
+        public void Initialize(IModuleConfiguration config)
+        {
+            config.LineProcessor<HttpLineProcessor>();
+        }
+    }
+}
